be3da36c0a2698e85ab7ca4ef26b409f1602f125
FaisalFs <51811615+faisalfs10x@users.noreply.github.com>
2021-11-18 16:23:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,65 +1,68 @@
-echo "Update kali repo"
+echo "Update kali repo\n"
 cat << EOF > /etc/apt/sources.list
 deb http://http.kali.org/kali kali-rolling main non-free contrib
 deb-src http://http.kali.org/kali kali-rolling main non-free contrib
 EOF
 
-echo "Set Google DNS"
+echo "Set Google DNS\n"
 cat << EOF > /etc/resolv.conf
 8.8.8.8
 8.8.4.4
 EOF
 
-echo "Update & Full-Upgrade"
+echo "Update & Full-Upgrade\n"
 apt update -y && apt -y full-upgrade -y
 
-echo "Install Tools"
+echo "Install Tools\n"
 apt-get install -y openssh-server responder macchanger voiphopper snmpcheck onesixtyone patator isr-evilgrade screen crackmapexec xrdp autossh sshpass
 
-echo "Enable RDP"
+echo "Enable RDP\n"
 service xrdp-sesman start
 systemctl enable xrdp
 
-echo "Setting up timezone to Asia/Singapore"
+echo "Setting up timezone to Asia/Singapore\n"
 timedatectl set-timezone 'Asia/Singapore'
 
-echo "Setting up hostname, pls insert new hostname"
+echo "Setting up hostname, pls insert new hostname\n"
 read -r CHG_HOSTNAME
 hostnamectl set-hostname $CHG_HOSTNAME
 
-echo "Setting up ssh-keygen to remote host"
+echo "Setting up ssh-keygen to remote host for user root\n"
 sudo -u root bash -c "ssh-keygen -f ~root/.ssh/id_rsa -N ''"
 
-echo "Please insert the password used for ssh login on remote machine:"
+echo "Please insert the password used for ssh login on remote machine, eg P@ssw0rd:\n"
 read -r USERPASS
 
-echo "Please insert the location of id_rsa.pub file, eg /root/.ssh/id_rsa.pub :"
+echo "Please insert the location of id_rsa.pub file, eg /root/.ssh/id_rsa.pub :\n"
 read -r KEYLOCATION
 
-echo "Please insert username remote host:"
+echo "Please insert username remote host, eg root:\n"
 read -r USER
 
-echo "Please insert IP of remote host:"
+echo "Please insert IP of remote host, eg 128.78.6.45:\n"
 read -r TARGETIP
 
-echo "Please insert ssh PORT of remote host:"
+echo "Please insert ssh PORT of remote host, eg 22:\n"
 read -r SSHPORT
 
 echo "$USERPASS" | sshpass ssh-copy-id -f -i $KEYLOCATION "$USER"@"$TARGETIP"
 
 echo "Setting up systemd service as autossh-ptunnel.service:"
+echo "Please insert systemd file name, eg tunnel or revtunnel:\n"
+read -r SYSTEMD_NAME
+echo "The systemd file will be in /etc/systemd/system/$SYSTEMD_NAME.service:\n"
 
-echo "Please insert ssh tunnel -R PORT on remote host, eg 6666:"
-read -r SSHPORT
+echo "Please insert ssh tunnel -R PORT on remote host, eg 6666:\n"
+read -r RSSHPORT
 
-cat << EOF > /etc/systemd/system/autossh-ptunnel.service
+cat << EOF > /etc/systemd/system/$SYSTEMD_NAME.service
 [Unit]
 Description=AutoSSH tunnel service nyekeng-baru on remote port $SSHPORT
 After=network.target
 
 [Service]
 Environment="AUTOSSH_GATETIME=0"
-ExecStart=/usr/bin/autossh -M 11166 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -N -i $KEYLOCATION -R $SSHPORT:localhost:$SSHPORT $USER@$TARGETIP
+ExecStart=/usr/bin/autossh -M 11166 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -N -i $KEYLOCATION -R $RSSHPORT:localhost:$SSHPORT $USER@$TARGETIP
 
 [Install]
 WantedBy=multi-user.target
@@ -69,8 +72,9 @@ WantedBy=multi-user.target
 #ServerAliveCountMax - assume failure after 3 consecutive failed messages. Such configuration ensures a quick recovery after the connection failure.
 EOF
 
-echo "Reload daemon & start autossh-ptunnel.service:"
-
+echo "Reload daemon & start $SYSTEMD_NAME.service:\n"
 systemctl daemon-reload
 systemctl start autossh-ptunnel.service
 systemctl enable autossh-ptunnel.service
+
+
